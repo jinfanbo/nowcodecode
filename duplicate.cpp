eb33466dc31043838067d37dd625278ed2837bda
jinfanbo <jinfanbo0320@163.com>
2018-07-16 15:13:02 +0800

    nowcode duplicate

--- duplicate.cpp
+++ duplicate.cpp
@@ -3,13 +3,7 @@
 using namespace std;
 class Solution {
 public:
-    // Parameters:
-    //        numbers:     an array of integers
-    //        length:      the length of array numbers
-    //        duplication: (Output) the duplicated number in the array number
-    // Return value:       true if the input is valid, and there are some duplications in the array number
-    //                     otherwise false
-    bool duplicate(int numbers[], int length, int* duplication) {
+    bool duplicate(int numbers[], int length, int duplication) {
         if(numbers == NULL||length <= 0) {
             return false;
         }
@@ -18,6 +12,27 @@ public:
                 return false;
             }
         }
-        
+        for(int i = 0;i < length;i++) {
+            while(numbers[i]!=i) {
+                if(numbers[i] == numbers[numbers[i]]) {
+                    duplication = numbers[i];
+                    printf("%d\n", duplication);
+                    return true;
+                }
+                int temp = numbers[i];
+                numbers[i] = numbers[temp];
+                numbers[temp] = temp;
+            }
+        }
+        return false;
     }
 };
+
+int main() {
+    Solution A;
+    int a;
+    int numbers[] = {1,2,3,5,4,2,4,5,5,6};
+    A.duplicate(numbers, 10, a);
+    system("pause");
+    return 0;
+}
