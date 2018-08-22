f2f3321d2dbf8934b99f2ee9761230422c0a7cd9
jinfanbo <jinfanbo0320@163.com>
2018-08-23 07:45:21 +0800

    add

--- JewelsandStones.cpp
+++ JewelsandStones.cpp
@@ -29,3 +29,21 @@ int main()
     system("pause");
     return 0;
 }
+
+/*
+python3
+
+class Solution:
+    def numJewelsInStones(self, J, S):
+        """
+        :type J: str
+        :type S: str
+        :rtype: int
+        """
+        count = 0
+        for i in range(len(J)):
+            if J[i] in S:
+                count += S.count(J[i])
+        return count
+
+*/
