c0fc83d70f8cf7e5a2d876e428eca6845f028686
jinfanbo <jinfanbo0320@163.com>
2018-08-23 17:45:59 +0800

    2018-8-23

---
+++ ShellSort.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+// 分类 -------------- 内部比较排序
+// 数据结构 ---------- 数组
+// 最差时间复杂度 ---- 根据步长序列的不同而不同。已知最好的为O(n(logn)^2)
+// 最优时间复杂度 ---- O(n)
+// 平均时间复杂度 ---- 根据步长序列的不同而不同。
+// 所需辅助空间 ------ O(1)
+// 稳定性 ------------ 不稳定
+
+void ShellSort(int A[], int n)
+{
+    int h = 0;
+    while (h <= n)                          // 生成初始增量
+    {
+        h = 3 * h + 1;
+    }
+    while (h >= 1)
+    {
+        for (int i = h; i < n; i++)
+        {
+            int j = i - h;
+            int get = A[i];
+            while (j >= 0 && A[j] > get)
+            {
+                A[j + h] = A[j];
+                j = j - h;
+            }
+            A[j + h] = get;
+        }
+        h = (h - 1) / 3;                    // 递减增量
+    }
+}
+
+int main()
+{
+    int A[] = { 5, 2, 9, 4, 7, 6, 1, 3, 8 };// 从小到大希尔排序
+    int n = sizeof(A) / sizeof(int);
+    ShellSort(A, n);
+    printf("希尔排序结果：");
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", A[i]);
+    }
+    printf("\n");
+    return 0;
+}
